c06f0f1626faeeee65417487947586ef4ad1cd75
Hoda Khier <hoda7m@gmail.com>
2022-05-26 17:01:57 +0300

    password validator 1

---
+++ password-validator.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/bash
+echo "Enter a password"
+read user_password
+
+$Check_password = 0 #valid password
+
+Check_numbers()
+{
+    #This function checks if the password doesn't contain numbers
+    if [![$VAR =~ [0-9] ]]; then
+    echo "Password has no numbers"
+    #invalid password
+    return 1
+}
+
+Check_capitals()
+{
+    #This function checks if the password doesn't contain capital case letters
+    if [![^[A-Z]+$ ]]; then
+    echo "Password has no capital case letters"
+     #invalid password
+    return 1
+}
+
+
+Check_lowercase()
+{
+     #This function checks if the password doesn't contain lower case letters
+    if [![^[a-z]+$ ]]; then
+    echo "Password has no lower case letters"
+    #invalid password
+    return 1
+}
+
+password_length= ${#user_password[@]}
+
+if [$password_length -lt 10]; then
+    echo "Password length is under 10"
+    return 1
+else
+    Check_numbers()
+    Check_capitals()
+    Check_lowercase()
+
+
+
+ 
