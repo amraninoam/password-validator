e945d349df8893d1b41cb8dea1a6e9c2f8dfd534
Hoda Khier <hoda7m@gmail.com>
2022-05-26 17:49:13 +0300

    code updates 5

--- password-validator.sh
+++ password-validator.sh
@@ -6,36 +6,44 @@ Check_password = 0 #valid password
 function Check_numbers()
 {
     #This function checks if the password doesn't contain numbers
-    if [![$user_password =~ [0-9] ]]; then
+    if [![$user_password =~ [0-9] ]]
+     then
         echo "Password has no numbers"
         #invalid password
         return 1
+    fi
         }
 
 function Check_capitals()
 {
     #This function checks if the password doesn't contain capital case letters
-    if [![^[A-Z]+$ ]]; then
+    if [![^[A-Z]+$ ]]
+     then
         echo "Password has no capital case letters"
         #invalid password
         return 1
+    fi
 }
 
 
 function Check_lowercase()
 {
      #This function checks if the password doesn't contain lower case letters
-    if [![^[a-z]+$ ]]; then
+    if [![^[a-z]+$ ]]
+     then
         echo "Password has no lower case letters"
         #invalid password
         return 1
+    fi
 }
 
 password_length= ${#user_password[@]}
 
-if [$password_length -lt 10]; then
+if [$password_length -lt 10]
+ then
     echo "Password length is under 10"
     return 1
+fi
 else
     Check_numbers
     Check_capitals
