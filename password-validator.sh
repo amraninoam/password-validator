20e86e7f0f7fc58f52172920246ada748a666f5b
Hoda Khier <hoda7m@gmail.com>
2022-05-26 17:14:27 +0300

    code updates

--- password-validator.sh
+++ password-validator.sh
@@ -1,25 +1,24 @@
 #! /usr/bin/bash
-echo "Enter a password"
-read user_password
-
-$Check_password = 0 #valid password
+read $@
+user_password =$@
+Check_password = 0 #valid password
 
 Check_numbers()
 {
     #This function checks if the password doesn't contain numbers
-    if [![$VAR =~ [0-9] ]]; then
-    echo "Password has no numbers"
-    #invalid password
-    return 1
+    if [![$user_password =~ [0-9] ]]; then
+        echo "Password has no numbers"
+        #invalid password
+        return 1
 }
 
 Check_capitals()
 {
     #This function checks if the password doesn't contain capital case letters
     if [![^[A-Z]+$ ]]; then
-    echo "Password has no capital case letters"
-     #invalid password
-    return 1
+        echo "Password has no capital case letters"
+        #invalid password
+        return 1
 }
 
 
@@ -27,9 +26,9 @@ Check_lowercase()
 {
      #This function checks if the password doesn't contain lower case letters
     if [![^[a-z]+$ ]]; then
-    echo "Password has no lower case letters"
-    #invalid password
-    return 1
+        echo "Password has no lower case letters"
+        #invalid password
+        return 1
 }
 
 password_length= ${#user_password[@]}
